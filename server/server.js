2a334eaa23dfea092c554ebc901fa2ac032be306
SvetlanaDavydova <svetlana.davydova161@gmail.com>
2021-05-05 16:20:40 +0300

    TDL-0-initial-setup* setup database

--- server/server.js
+++ server/server.js
@@ -1,6 +1,16 @@
 const express = require ("express");
 const app = express();
 const PORT = 3000;
+
+
+const mysql = require("mysql2");
+const connection = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    database: "todolist",
+    password: "root"
+})
+connection.connect(()=> console.log("connect to database"));
 app.get("/", (req,res)=> {
     res.send("hello");
 })

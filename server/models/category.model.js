ce113628b77e25247b4df41df1d7ff803ff4f97d
Svetlana Davydova <svetlana.davydova161@gmail.com>
2021-05-27 21:43:18 +0300

    TDL-3-async-await-attempt * async await in categories model

--- server/models/category.model.js
+++ server/models/category.model.js
@@ -1,46 +1,30 @@
-const mysql = require("mysql2");
-const connection = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    database: "todolist",
-    password: "root"
-});
-connection.connect(()=> console.log("connect to database"));
+const {getConnection} = require("../config/dbconfig");
 
-
-exports.get = async function(){
-    return new Promise(function(resolve, reject){
-        connection.query("SELECT * FROM category", (err, results, fields) => {
-            if(err) reject(err);
-            resolve(results);
-        });
-    });
-};
+exports.get =  async function(){
+    const connection = getConnection();
+    const query = "SELECT * FROM category";
+    const [rows, fields] = await connection.execute(query);
+    return rows;
+}
 
 exports.getById = async function(id){
-    return new Promise(function(resolve, reject){
-        connection.query("SELECT * FROM category WHERE id = (?)", [id], (err, results, fields) => {
-            if(err) reject(err);
-            resolve(results[0]);
-        });
-    });
-};
+    const connection = getConnection();
+    const query = "SELECT * FROM category WHERE id = (?)";
+    const [rows, fields] = await connection.execute(query,[id] );
+    return rows;
+}
 
-exports.create = async function(catName){
-    return new Promise(function(resolve, reject){
-        connection.query("INSERT INTO category (cat_name) VALUES (?)", [catName], (err, results, fields) => {
-            if(err) reject(err);
-            resolve(results);
-        });
-    });
-};
+exports.create = async function(title,status){
+    const connection = getConnection();
+    const query = "INSERT INTO category (cat_name) VALUES (?)";
+    const [rows, fields] = await connection.execute(query,[catName]);
+    return rows;
+}
 
 exports.deleteCategory = async function(id){
-    return new Promise(function(resolve, reject){
-        connection.query("DELETE FROM category WHERE id = (?)", [id], (err, results, fields) => {
-            if(err) reject(err);
-            resolve(results[0]);
-        });
-    });
+    const connection = getConnection();
+    const query = "DELETE FROM category WHERE id = (?)";
+    const [rows, fields] = await connection.execute(query,[id] );
+    return rows;
 };
 
